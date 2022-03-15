10dece1b5391926cd7a41ec3c0c6be16fd2df751
Ranjeetpoojari <ranjeetpoojari77@gmail.com>
2022-03-15 22:09:20 +0530

    changes

--- Scheduler.js
+++ Scheduler.js
@@ -15,21 +15,18 @@ function showme()
   }
 }
 
-// var b ;
-// function dis()
-// {
-//   if (b==1)
-//   {
-//     document.getElementById("Timer").style.display="inline";
-//     document.getElementById("Price").style.display="inline";
-//     document.getElementById("Percentage1").style.display="inline";
-//     return b=0;
-//   }
-//   else
-//   {
-//     document.getElementById("Timer").style.display="none";
-//     document.getElementById("Price").style.display="none";
-//     document.getElementById("Percentage1").style.display="none"; 
-//     return b=1
-//   }
-// }
+var b;
+function password()
+{
+    if(b==1)
+    {
+        document.getElementById("password").type="password";
+        return b=0;
+        
+    }
+    else
+    {
+        document.getElementById("password").type="text";
+        return b=1;
+    }
+}
